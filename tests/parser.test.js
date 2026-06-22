333b21030be244f5cb5c799860bcc4f8751f8384
macByteCodex <macByteCodex@local>
2026-06-22 16:25:08 +0800

    Filter public recruitment notices

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -2,11 +2,12 @@ const test = require("node:test");
 const assert = require("node:assert/strict");
 const { parseNotices } = require("../src/server");
 
-test("parseNotices extracts official notice cards", () => {
+test("parseNotices keeps recruitment notices and excludes public notices", () => {
   const html = `
     <ul>
       <li><a href="./202603/t20260303_1.html">天津市部分事业单位公开招聘信息</a> 2026-03-03</li>
       <li><a href="/jsdw/rsksw/tzgg4/202602/t20260226_2.html">天津市滨海新区2025年公开招考公务员拟录用人员公示</a>[02-26]</li>
+      <li><a href="/sydw.html">天津市某单位2026年公开招聘工作人员拟聘用名单</a>[02-25]</li>
       <li><a href="/abc.html">关于2026年度专业技术人员职业资格考试有关事项的通知</a>[02-05]</li>
     </ul>
   `;
@@ -19,9 +20,9 @@ test("parseNotices extracts official notice cards", () => {
     categoryHints: [["公务员", "公务员"]]
   });
 
-  assert.equal(notices.length, 2);
+  assert.equal(notices.length, 1);
   assert.equal(notices[0].category, "事业单位");
   assert.equal(notices[0].date, "2026-03-03");
-  assert.equal(notices[1].category, "公务员");
   assert.match(notices[0].url, /^https:\/\/hrss\.tj\.gov\.cn/);
+  assert.ok(notices.every((notice) => !notice.title.includes("公示")));
 });
